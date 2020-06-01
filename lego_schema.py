6c6b0e386cb8d06cc827b28644961f102ddb9890
Dorothy Li <qli22@student.gsu.edu>
2020-06-01 17:50:02 -0400

    CREATE SCHEMA FOR LEGO_SCRAPER

---
+++ lego_schema.py
@@ -0,0 +1,22 @@
+'''
+lego_schema.py
+---------------
+Dorothy Li
+June 1 2020
+Lego Scraper Schema
+'''
+
+SCHEMA = [
+    'item_name',
+    'item_id',
+    'theme',
+    'sub_theme',
+    'rating',
+    'price',
+    'num_pieces',
+    'num_minifigs',
+    'release_date',
+    'release_year',
+    'market_price',
+    'image'
+    ]
